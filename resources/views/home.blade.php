fc4737ce885ade9d6040b0f44d3dfb32726a7174
Swaraj <swarajchbiswas44@gmail.com>
2023-06-17 02:23:46 +0600

    username show done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 
 <html lang="en">
@@ -28,131 +27,125 @@
 
   <header>
     <div class="logo">
-    <img src="{{ asset('Pictures/University_of_Oxford-Logo.wine.svg') }}" alt="University Logo" width="250px" height="100px">
-      
+    <img src="{{ asset('Pictures/University_of_Oxford-Logo.wine.svg') }}" alt="University Logo"  width="150px" height="100px">
     
     </div>
     <style>
-      headdrop {
-        background-color: rgb(0, 0, 0);
-      }
+      
+headdrop {
+  background-color: rgb(0, 0, 0);
+}
 
-      div a {
-        text-decoration: none;
-        color: white;
-        font-size: 15px;
-        padding: 15px;
-        display: inline-block;
-      }
+div a {
+  text-decoration: none;
+  color: white;
+  font-size: 15px;
+  padding: 15px;
+  display: inline-block;
+}
 
-      ul {
-        display: inline;
-        margin: 0;
-        padding: 0;
-      }
+ul {
+  display: inline;
+  margin: 0;
+  padding: 0;
+}
 
-      ul li {
-        display: inline-block;
-      }
+ul li {
+  display: inline-block;
+}
 
-      ul li:hover {
-        background: #555;
-      }
+ul li:hover {
+  background: #555;
+}
 
-      ul li:hover ul {
-        display: block;
-      }
+ul li:hover ul {
+  display: block;
+}
 
-      ul li ul {
-        position: absolute;
-        width: 200px;
-        display: none;
-      }
+ul li ul {
+  position: absolute;
+  width: 200px;
+  display: none;
+}
 
-      ul li ul li {
-        background: #555;
-        display: block;
-      }
+ul li ul li {
+  background: #555;
+  display: block;
+}
 
-      ul li ul li a {
-        display: block !important;
-      }
+ul li ul li a {
+  display: block !important;
+}
 
-      ul li ul li:hover {
-        background: #666;
-      }
+ul li ul li:hover {
+  background: #666;
+}
     </style>
 
 
-    <div class="headdrop">
-      <a href="home.html">Home</a>
+<div class="headdrop">
 
-      <a href="About us.html">About Us</a>
-
-
+<a href="home.php">Home</a>
+<a href="About us.php">About Us</a>
 
 
-      <ul>
-        <li>
-          <a>Admissions</a>
-          <ul>
-            <li><a href="file:///C:/Users/DELL/Desktop/New%20folder%20(5)/test.html#">Undergraduate</a></li>
-            <li><a href="file:///C:/Users/DELL/Desktop/New%20folder%20(5)/test.html#">Graduate</a></li>
-            <li><a href="file:///C:/Users/DELL/Desktop/New%20folder%20(5)/test.html#">Continuing Education</a></li>
-          </ul>
-        </li>
+
+
+<ul>
+  <li>
+    <a>Admissions</a>
+    <ul>
+      <li><a href="file:///C:/Users/DELL/Desktop/New%20folder%20(5)/test.html#">Undergraduate</a></li>
+      <li><a href="file:///C:/Users/DELL/Desktop/New%20folder%20(5)/test.html#">Graduate</a></li>
+      <li><a href="file:///C:/Users/DELL/Desktop/New%20folder%20(5)/test.html#">Continuing Education</a></li>
+    </ul>
+  </li>
+</ul>
+
+
+<ul>
+  <li>
+    <a >Academics</a>
+    <ul>
+      <li><a href="file:///C:/Users/DELL/Desktop/New%20folder%20(5)/test.html#">Undergraduate</a></li>
+      <li><a href="file:///C:/Users/DELL/Desktop/New%20folder%20(5)/test.html#">Graduate</a></li>
+      <li><a href="file:///C:/Users/DELL/Desktop/New%20folder%20(5)/test.html#">Post Graduate</a></li>
       </ul>
-
-
-      <ul>
-        <li>
-          <a >Academics</a>
-          <ul>
-            <li><a href="file:///C:/Users/DELL/Desktop/New%20folder%20(5)/test.html#">Undergraduate</a></li>
-            <li><a href="file:///C:/Users/DELL/Desktop/New%20folder%20(5)/test.html#">Graduate</a></li>
-            <li><a href="file:///C:/Users/DELL/Desktop/New%20folder%20(5)/test.html#">Post Graduate</a></li>
-            </ul>
-        </li>
-      </ul>
+  </li>
+</ul>
 
 
 
-      <ul>
-        <li>
-          <a>Research</a>
-          <ul>
-            <li><a href="file:///C:/Users/DELL/Desktop/New%20folder%20(5)/test.html#">Engage With us</a></li>
-            <li><a href="file:///C:/Users/DELL/Desktop/New%20folder%20(5)/test.html#">Support for researchers</a></li>
-            <li><a href="file:///C:/Users/DELL/Desktop/New%20folder%20(5)/test.html#">Recognition</a></li>
-          </ul>
-        </li>
-      </ul>
+<ul>
+  <li>
+    <a>Research</a>
+    <ul>
+      <li><a href="file:///C:/Users/DELL/Desktop/New%20folder%20(5)/test.html#">Engage With us</a></li>
+      <li><a href="Publish_paper.php">Publish Papers</a></li>
+      <li><a href="article.php">Research Papers</a></li>
+    </ul>
+  </li>
+</ul>
 
 
-      <ul>
-          <li>
-              <a>Authenticate</a>
-              <ul>
-                  <li><a href="loginpage.html">Log in</a></li>
-                  <li><a href="Regpage.html">Create Account</a></li>
-                 
-              </ul>
-          </li>
-      </ul>
+<ul>
+    <li>
+        <a>Authenticate</a>
+        <ul>
+            <li><a href="loginpage.php">Log in</a></li>
+            <li><a href="Regpage.php">Create Account</a></li>
+           
+        </ul>
+    </li>
+</ul>
 
-      <ul>
-          <li>
-              <a> {{ Auth::user()->name }} </a>
-          </li>
-      </ul>
+<a href="contact_us.php">Contact Us</a>
 
-      <a href="contact_us.html">Contact Us</a>
+<a> {{ Auth::user()->name }} </a>
 
 
 
-    </div>
-
-
+</div>
 
 
 
